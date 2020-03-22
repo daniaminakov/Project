61ca027a808468778f5fe2c8fe1ed446c500a7d9
Danil <danilminakov333@gmail.com>
2020-03-22 11:35:44 +0200

    22.03.2020

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -21,17 +21,17 @@ document.querySelector('.burger-menu').addEventListener('click', menuClick);
 
 
 // scrollup-START
-// $(window).scroll(function() {
-//     if ($(this).scrollTop() > 1000) {
-//         $('.scrollup').fadeIn();
-//     } else {
-//         $('.scrollup').fadeOut();
-//     }
-// });
-// $('.scrollup').click(function() {
-//     $("html, body").animate({ scrollTop: 0 }, 1000);
-//     return false;
-// });
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 500) {
+        $('.scrollup').fadeIn();
+    } else {
+        $('.scrollup').fadeOut();
+    }
+});
+$('.scrollup').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+    return false;
+});
 // scrollup-END
 
 
@@ -46,3 +46,69 @@ $(".arrow-bottom").on("click", function(event) {
     //анимируем переход на расстояние - top за 1000 мс
     $('body,html').animate({ scrollTop: top }, 1000);
 });
+
+// Прогресс-START
+
+var target = $('.skills-progress');
+var targetPos = target.offset().top;
+var winHeight = $(window).height();
+var scrollToElem = targetPos - winHeight;
+$(window).scroll(function() {
+    var winScrollTop = $(this).scrollTop();
+    if (winScrollTop > scrollToElem) {
+        progressSkills();
+    }
+});
+
+function progressSkills() {
+    var progressBar = document.querySelectorAll(".progress-bar");
+    var time = 2000;
+    progressBar.forEach(function(i) {
+        let label = i.children[1].children[0].children[0];
+        let line = i.children[1];
+        let count = 0;
+        let dataCount = label.getAttribute("data-count");
+        let lineCount = line.children[0];
+        let runTime = time / dataCount;
+        let animationLineCount = setInterval(function() {
+            if (count < dataCount) {
+                count++;
+                label.innerHTML = count + '%';
+                lineCount.style.width = count + '%';
+            }
+        }, runTime);
+    });
+    progressSkills = function() {};
+};
+
+// Прогресс-END
+
+
+// works-cards-START
+
+let card = new Card('js-card');
+card.run();
+
+function Card(classCard) {
+    this.cards = document.querySelectorAll('.' + classCard);
+    this.bindEventsCard = function() {
+        for (let i = 0, length = this.cards.length; i < length; i++) {
+            this.cards[i].addEventListener('mousemove', this.startRotate);
+            this.cards[i].addEventListener('mouseout', this.stopRotate);
+        }
+    }
+    this.startRotate = function(event) {
+        let cardItem = this.querySelector('.card-item'),
+            halfHeight = cardItem.offsetHeight / 2,
+            halfWidth = cardItem.offsetWidth / 2;
+        cardItem.style.transform = 'rotatex(' + -(event.offsetY - halfHeight) / 10 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 15 + 'deg)';
+    }
+    this.stopRotate = function(event) {
+        let cardItem = this.querySelector('.card-item');
+        cardItem.style.transform = 'rotate(0)';
+    }
+    this.run = () => {
+        this.bindEventsCard();
+    }
+}
+// works-cards-END
